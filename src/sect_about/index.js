201fc931f1b45a5eebb27bb31195a3f6753dd642
Aaron Todd <aaron.todd@hthackney.com>
2018-07-31 08:00:30 -0400

    Centers title for mobile friendliness

--- src/sect_about/index.js
+++ src/sect_about/index.js
@@ -10,7 +10,8 @@ const About = () => (
             <img src={BSALogo} alt="BSA Logo" />
           </div>
             <div className="col-md-10">
-              <h3>About Scouting</h3>
+              <h3 className="d-none d-sm-block">About Scouting</h3>
+              <h3 className="d-block d-sm-none" style={{textAlign:'center'}}>About Scouting</h3>
               <h4>The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law.</h4>
             </div>
         </div>
--- src/sect_contact/index.js
+++ src/sect_contact/index.js
@@ -6,7 +6,8 @@ const Contact = () => (
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
-                    <h3>Contact Us</h3>
+                    <h3 className="d-none d-sm-block">Contact Us</h3>
+                    <h3 className="d-block d-sm-none" style={{textAlign:'center'}}>Contact Us</h3>
                     <h4>We'd love to hear from you and will be happy to answer any questions you have.  Please email <a href="mailto:info@pack45.org">info@pack45.org</a> with questions.</h4>
                 </div>
             </div>
