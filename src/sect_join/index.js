a7cdfa37f74ead07874cd37c564af425ec7c8835
Aaron Todd <aaron.todd@hthackney.com>
2018-07-31 08:07:49 -0400

    New art and colors

--- src/sect_join/index.js
+++ src/sect_join/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './join.css';
 import { Link } from 'react-router-dom';
+import BSALogo from './join.png';
 
 const Join = () => (
-    <section className="join">
+    <section className="join_sect">
         <div className="container">
         <div className="row">
-          <div className="col-md-12">
-            <h3>Join Cub Scouts!</h3>
+          <div className="col-md-4 text-center">
+            <img src={BSALogo} alt="BSA Logo" />
+          </div>
+          <div className="col-md-8">
+              <h3 className="d-none d-sm-block">Join Cub Scouts!</h3>
+              <h3 className="d-block d-sm-none" style={{textAlign:'center'}}>Join Cub Scouts!</h3>
             <h4>We'd love for your children to join in our fun, and we welcome them to join Pack 45 any time throughout the year.
               Feel free to attend our Pack meetings at First Congregational Church in Charlotte to learn more about us and what opportunities we can offer.
               They will not be disappointed!
