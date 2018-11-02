ffc198f71d0427966da7dc9fe86ab964fe84e84b
Aaron Todd <aaron.todd@hthackney.com>
2018-11-02 08:26:07 -0400

    Deals with joining info without a set recruitment meeting scheduled

--- src/page_join/index.js
+++ src/page_join/index.js
@@ -14,7 +14,7 @@ const JoinInfo = () => (
 	<section className="join">
 		<div className="container">
 			<div className="row">
-				<div className="col-md-2 text-center">
+				{/* <div className="col-md-2 text-center">
 					{iconCalendar}
 				</div>
 				<div className="col-md-10">
@@ -23,6 +23,16 @@ const JoinInfo = () => (
 					<h5>We invite you and your children (boys and girls) to join us on Thursday, September 6, 2018 at the First Congregational Church in Charlotte for an informational meeting about joining Cub Scouts with Pack 45.
 						We want you, your children, and your family to be completely comfortable with joining our Pack before we collect any fees or file your registration.</h5>
 					<h5>Elegible children should be going into kindergarden through 5th grade in the Fall of 2018.</h5>
+					<h5>We would love to meet you in order to provide a rundown of our yearly events and activities as well as answer any questions or concerns that you may have.</h5>
+					<h4>We look forward to visiting with you!</h4>
+				</div> */}
+				<div className="col-md-12">
+					<h3 className="d-none d-sm-block">Joining Cub Scouts</h3>
+					<h3 className="d-block d-sm-none" style={{textAlign:'center'}}>Joining Cub Scouts</h3>
+					<h5>We welcome new members any time of the year.
+						Please get in touch with us via email at <a href="mailto:info@pack45.org">info@pack45.org</a> for more information or to schedule a visit.
+						We want you, your children, and your family to be completely comfortable with joining our Pack before we collect any fees or file your registration.</h5>
+					<h5>Elegible children should be going into kindergarden through 5th grade in the Fall of the current year.</h5>
 					<h5>We would love to meet you in order to provide a rundown of our yearly events and activities as well as answer any questions or concerns that you may have.</h5>
 					<h4>We look forward to visiting with you!</h4>
 				</div>
@@ -46,16 +56,16 @@ const JoinInfo = () => (
 // );
 
 class JoinPage extends Component {
-	render() { 
+	render() {
 		window.scrollTo(0, 0);
-		return (  
+		return (
 			<div>
-				<Header /> 
+				<Header />
 				<JoinInfo />
 				<Footer />
 			</div>
 		)
 	}
 }
- 
+
 export default JoinPage;
