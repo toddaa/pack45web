dde62b55683fb6adfd050cf5cd81e3002c3d44fd
Aaron Todd <toddaa@gmail.com>
2021-11-25 10:43:51 -0500

    Test Link

--- src/sect_footer/index.js
+++ src/sect_footer/index.js
@@ -3,6 +3,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faCopyright from '@fortawesome/fontawesome-free-regular/faCopyright'
 import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope'
 import faFacebookSquare from '@fortawesome/fontawesome-free-brands/faFacebookSquare'
+import { Link } from 'react-router-dom'
 import './footer.css'
 import CubLogoSMI from './CubScout_SMI_Stacked_RGB.png'
 
@@ -23,11 +24,13 @@ const Footer = () => (
       <p className='d-none d-sm-block'>First Congregational Church | 106 Bostwick Street, Charlotte, MI</p>
       <p className='d-none d-sm-block'>{iconCopyright} Copyright 2016 | Cub Scout Pack 45 | All Rights Reserved</p>
       <p className='d-none d-sm-block'><a href='mailto:info@pack45.org'>{iconEnvelope} info@pack45.org</a> | <a href='http://facebook.com/pack45bsa'>{iconFacebook} @pack45bsa</a></p>
+      <p className='d-none d-sm-block'><Link to='/stuff'>Stuff</Link></p>
       <img className='d-block d-sm-none' src={CubLogoSMI} alt='Cub Scout Logo' style={{ width: '300px' }} />
       <h2 className='d-block d-sm-none'>Cub Scout Pack 45<br />Charlotte, Michigan</h2>
       <p className='d-block d-sm-none'>First Congregational Church<br />106 Bostwick Street, Charlotte, MI</p>
       <p className='d-block d-sm-none'>{iconCopyright} Copyright 2016 | Cub Scout Pack 45<br />All Rights Reserved</p>
       <p className='d-block d-sm-none'><a href='mailto:info@pack45.org'>{iconEnvelope} info@pack45.org</a><br /><a href='http://facebook.com/pack45bsa'>{iconFacebook} @pack45bsa</a></p>
+      <p className='d-block d-sm-none'><Link to='/stuff'>Stuff</Link></p>
     </div>
   </footer>
 )
